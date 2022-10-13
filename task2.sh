ed5d8b597933f9db4dfe3bbfdd00691f3fb5fa7f
toxanes <toxanes@gmail.com>
2022-10-13 16:00:50 +0200

    Add files

---
+++ task2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+read line
+echo $line | tr " " "\n"
---
+++ task3.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+read line
+if [[ "$line" == "a" ]]; then
+	    echo "Matched"
+    else
+	        echo "Not Matched"
+fi 
